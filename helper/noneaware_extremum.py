278927390bf7ff2de487e62f13b0d0aaeb357efa
Daanbe <uniskarsuni@gmail.com>
2022-01-04 21:47:17 +0100

    Add Docstrings to noneaware_extremum.py

--- helper/noneaware_extremum.py
+++ helper/noneaware_extremum.py
@@ -1,4 +1,8 @@
 def n_min(a,b):
+    """
+    Special min for comparison of two values which could include `None`.
+    `None` is only chosen if there is no other value. 
+    """
     if a is None:
         return b
     if b is None:
@@ -6,6 +10,10 @@ def n_min(a,b):
     return min(a,b)
 
 def n_max(a,b):
+    """
+    Special max for comparison of two values which could include `None`.
+    `None` is only chosen if there is no other value. 
+    """
     if a is None:
         return b
     if b is None:
